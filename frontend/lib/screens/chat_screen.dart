687fed5a1e5e640a763cee0fbf1d7b9969079306
Akshat-sGit <e21cseu0806@bennett.edu.in>
2025-01-05 13:13:02 +0530

    refactor: improve MessageBubble styling and update ChatScreen layout

--- frontend/lib/screens/chat_screen.dart
+++ frontend/lib/screens/chat_screen.dart
@@ -8,45 +8,67 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
         backgroundColor: Colors.black,
-        title: const Text('AI Chatbot', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Expanded(
-            child: ListView(
-              children: const [
-                ListTile(
-                  title: Text('Hello', style: TextStyle(color: Colors.white),),
-                  subtitle: Text('Hi', style: TextStyle(color: Colors.white),),
-                  leading: CircleAvatar(
-                    child: Text('AI'),
-                  ),
-                ),
-                ListTile(
-                  title: Text('How are you?', style: TextStyle(color: Colors.white),),
-                  subtitle: Text('I am fine', style: TextStyle(color: Colors.white),),
-                  leading: CircleAvatar(
-                    child: Text('AI'),
-                  ),
-                ),
-                ListTile(
-                  title: Text('What is your name?', style: TextStyle(color: Colors.white),),
-                  subtitle: Text('I am AI', style: TextStyle(color: Colors.white),),
-                  leading: CircleAvatar(
-                    child: Text('AI'),
-                  ),
-                ),
-                MessageBubble(isMe: true, message: "hi hello how are you"),
-              ],
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'AI Chatbot',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
-          const ChatInput(), 
-        ],
-      )
-    );
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              child: ListView(
+                children: const [
+                  ListTile(
+                    title: Text(
+                      'Hello',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      'Hi',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    leading: CircleAvatar(
+                      child: Text('AI'),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'How are you?',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      'I am fine',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    leading: CircleAvatar(
+                      child: Text('AI'),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'What is your name?',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      'I am AI',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    leading: CircleAvatar(
+                      child: Text('AI'),
+                    ),
+                  ),
+                  MessageBubble(isMe: true, message: "hi hello how are you"),
+                  MessageBubble(
+                      isMe: false, message: "Hello I am not fine I am just ai"),
+                ],
+              ),
+            ),
+            const ChatInput(),
+          ],
+        ));
   }
 }
--- frontend/lib/widgets/message_bubble.dart
+++ frontend/lib/widgets/message_bubble.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  
-  final String message; 
-  final bool isMe; 
+  final String message;
+  final bool isMe;
 
   const MessageBubble({super.key, required this.isMe, required this.message});
 
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft, 
+      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
-        padding:const EdgeInsets.all(10), 
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10), 
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
         decoration: BoxDecoration(
-          color: isMe ? Colors.blue : Colors.green,
-          borderRadius:const BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15), bottomLeft: Radius.circular(15)), 
+          color: isMe ? Colors.blue : Colors.green[400],
+          borderRadius: isMe
+              ? const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                  bottomLeft: Radius.circular(15))
+              : const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                  bottomRight: Radius.circular(15)),
         ),
         child: Text(
-          message, 
-          style: TextStyle(color: isMe ? Colors.white: Colors.black),
+          message,
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
     );
