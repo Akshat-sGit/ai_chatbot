0a2aafe93a24489257675eb3aca328607d0b099c
Akshat-sGit <e21cseu0806@bennett.edu.in>
2025-01-19 22:20:27 +0530

    feat: add flutter_riverpod dependency and wrap MyApp in ProviderScope

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,13 +3,18 @@ import 'package:ai_chatbot/firebase_options.dart';
 import 'package:ai_chatbot/screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
